af10f20fc93cc85b27e2c667387dc401772f2b02
Nicholas Felt <nicholas.felt@tektronix.com>
2025-07-08 10:57:15 -0700

    test: skip 1G waveform size test in GitHub Actions environment

--- tests/test_tm_data_types.py
+++ tests/test_tm_data_types.py
@@ -1,5 +1,6 @@
 """Tests for tm_data_types."""
 
+import os
 import timeit
 
 from pathlib import Path
@@ -544,6 +545,9 @@ def test_transforms(tmp_path: Path) -> None:
 )
 def test_wfm_size(tmp_path: Path, si_unit: str, length: int) -> None:
     """Test how different waveform sizes function efficiently."""
+    if si_unit in {"1G"} and os.getenv("GITHUB_ACTIONS"):
+        pytest.skip(f"Skipping {si_unit} test in GitHub Actions environment")
+
     waveform_path = tmp_path / f"test_length_{length}.wfm"
 
     # Generate data
