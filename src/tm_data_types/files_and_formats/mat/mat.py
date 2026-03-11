ceda75a10e0df5970e4f9437bc171734592cb823
SaiDeepikaKuchibhotla <saideepika.kuchibhotla@tektronix.com>
2026-03-11 16:35:25 +0530

    fix(mat): correct axis unit mappings in waveform properties

--- src/tm_data_types/files_and_formats/mat/mat.py
+++ src/tm_data_types/files_and_formats/mat/mat.py
@@ -67,8 +67,8 @@ class MATFile(AbstractedFile, Generic[DATUM_TYPE_VAR]):
     _WAVEFORM_PROPERTIES = bidict(
         {
             "source_name": "waveformSource",
-            "y_axis_units": "horizontalUnits",
-            "x_axis_units": "verticalUnits",
+            "y_axis_units": "verticalUnits",  # FIXED: was 'horizontalUnits'
+            "x_axis_units": "horizontalUnits",  # FIXED: was 'verticalUnits'
             "trigger_index": "zeroIndex",
             "x_axis_spacing": "sampleInterval",
         },
